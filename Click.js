2eeee4b369f824e9488582a9d24e58339fa47d6c
jaiderarevalo <jaiderruizx12@gmail.com>
2023-05-15 15:59:10 -0500

    buscador peliculas

---
+++ Click.js
@@ -0,0 +1,46 @@
+document.querySelector("#searchButton").addEventListener("click", buscarPeliculas);
+
+async function buscarPeliculas() {
+  const searchTerm = document.querySelector("#buscar").value;
+
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=f029c42c6c90fc64160358f3599ce822&query=${searchTerm}`
+    );
+    const data = await response.json();
+    console.log(data.results);
+    
+    const peliculasContainer = document.querySelector("#pelicula");
+    peliculasContainer.innerHTML = ""; // Limpiar resultados anteriores
+
+    if (data.results.length === 0) {
+
+      const divs = document.createElement("div");
+      const mensaje = document.createElement("p");
+      mensaje.textContent = "No se encontraron películas con ese término de búsqueda.";
+      peliculasContainer.appendChild(mensaje);
+      peliculasContainer.appendChild(divs);
+      mensaje.classList.add("text-white","bg-red-500","w-full","mt-10")
+      divs.classList.add("text-white","bg-blue-500","w-full","mt-10")
+    } else {
+      data.results.forEach((pelicula) => {
+        if (pelicula.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+          const peliculaDiv = document.createElement("div");
+          peliculaDiv.classList.add("pelicula-item");
+
+          const titulo = document.createElement("h3");
+          titulo.textContent = pelicula.title;
+
+          const imagen = document.createElement("img");
+          imagen.src = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`;
+
+          peliculaDiv.appendChild(titulo);
+          peliculaDiv.appendChild(imagen);
+          peliculasContainer.appendChild(peliculaDiv);
+        }
+      });
+    }
+  } catch (error) {
+    console.log("Ha ocurrido un error:", error);
+  }
+}
---
+++ api.js
@@ -0,0 +1,53 @@
+
+(async function pelis() {
+  try {
+    const response = await fetch(
+      "https://api.themoviedb.org/3/discover/movie?api_key=f029c42c6c90fc64160358f3599ce822"
+    );
+    const datas = await response.json();
+    console.log(datas);
+
+    const divContainer = document.querySelector("#pelicula");
+    divContainer.classList.add("grid", "grid-cols-4", "gap-4");
+
+    datas.results.map((data) => {
+      const div = document.createElement("div");
+      const titulos = document.createElement("h1");
+      const imagen = document.createElement("img");
+      const Descripcion = document.createElement("p");
+
+      
+    div.classList.add("pt-2", "px-2", "border-4", "border-white");
+      titulos.classList.add(
+        "text-lg",
+        "text-center",
+        "rounded-b-xl",
+        "text-white",
+        "bg-black",
+        "h-auto",
+        "font-bold",
+        "mb-2"
+      );
+      imagen.classList.add("w-full", "h-auto");
+      Descripcion.classList.add("text-white","bg-black","h-auto")
+      titulos.textContent = data.title;
+      imagen.src = `https://image.tmdb.org/t/p/w500/${data.poster_path}`;
+      Descripcion.textContent = data.overview;
+
+           imagen.addEventListener("click", () => {
+        const searchQuery = `${data.title} official trailer`;
+        const youtubeURL = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+          searchQuery
+        )}`;
+        window.open(youtubeURL, "_blank");
+      });
+      divContainer.appendChild(div);
+      div.appendChild(titulos);
+      div.appendChild(imagen);
+      div.appendChild(Descripcion);
+    });
+  } catch (error) {
+    "hay varios errores;", error;
+  }
+})();
+
